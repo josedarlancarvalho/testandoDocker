aa4691cf1ef107300660b172a614f35bc1295f39
Darlan Carvalho <137117083+josedarlancarvalho@users.noreply.github.com>
2024-05-29 10:26:05 -0300

    Add files via upload

---
+++ asd/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const h1 = document.querySelector("h1");
+    h1.textContent = "Teste do Docker Bem-Sucedido";
+
+    const p = document.querySelector("p");
+    p.textContent = "Usando isso para testar a configuração do Docker.";
+});
